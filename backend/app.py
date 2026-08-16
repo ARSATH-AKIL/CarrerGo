93b6e821d1111490ea4de400b5cac8bfcdbe569b
ARSATH-AKIL <arsathakilmsd@gmail.com>
2026-08-16 19:27:39 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -22,7 +22,7 @@ CORS(
 
 DB_CONFIG = {
     "host": os.getenv("DB_HOST"),
-    "port": int(os.getenv("DB_PORT", "3306")),
+    "port": int(os.getenv("DB_PORT")),
     "user": os.getenv("DB_USER"),
     "password": os.getenv("DB_PASSWORD"),
     "database": os.getenv("DB_NAME")
