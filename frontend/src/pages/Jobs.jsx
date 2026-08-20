ebbfa893e6c10c7d1b7225e5500421a314b7e5d4
ARSATH-AKIL <arsathakilmsd@gmail.com>
2026-08-20 09:55:46 +0530

    Remove job description from Jobs page
    Removed job description display from the Jobs page.

--- frontend/src/pages/Jobs.jsx
+++ frontend/src/pages/Jobs.jsx
@@ -181,9 +181,7 @@ function Jobs() {
                     <span>💼 {job.type}</span>
                   </div>
 
-                  <p className="job-description">
-                    {job.description}
-                  </p>
+                
 
                   <p className="job-skills">
                     <strong>Skills:</strong> {jobSkills}
